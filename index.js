8e4835f7be31089bf12fd435c99285ac36b1b45c
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-29 08:27:19 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
+
+function isPalindrome(word) {
+  // Write your algorithm 
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+ 
 
 /* 
   Add your pseudocode here
@@ -17,6 +29,27 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
